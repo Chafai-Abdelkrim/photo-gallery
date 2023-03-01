77f36b8459e76a3770a708e9455f6d5c6e572ecf
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-01 14:30:42 +0100

    added: Title component with styles

---
+++ src/components/Title.tsx
@@ -0,0 +1,19 @@
+import { BiPhotoAlbum } from 'react-icons/bi'
+
+const Title = () => {
+  return (
+    <div className="Title">
+      <div className="header">
+          <BiPhotoAlbum />
+          <h1>FireGallery</h1>
+      </div>
+      <h2>Your Photos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis repellat
+        placeat.
+      </p>
+    </div>
+  );
+};
+
+export default Title;
